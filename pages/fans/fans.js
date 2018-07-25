346b2d1c05ab786b748b152c5dadeabff63fd0b3
gyy <1228098106@qq.com>
2018-07-25 16:16:33 +0800

    关于我们

---
+++ pages/fans/fans.js
@@ -0,0 +1,58 @@
+const app = getApp()
+var util = require('../../utils/util.js');
+var $loading
+Page({
+  data: {
+    wallet: [],
+    id: null,
+    name: null,
+    avatar: null
+  },
+  onLoad: function (options) {
+    $loading = this.selectComponent(".J_loading")
+    $loading.show()
+  },
+  onShow: function () {
+    if (app.globalData.access_token) {
+      this.getData()
+    } else {
+      wx.login({
+        success: res => {
+          if (res.code) {
+            //发起网络请求
+            wx.request({
+              url: `${app.globalData.host}/api/auth/login`,
+              method: 'POST',
+              data: {
+                code: res.code
+              },
+              success: res => {
+                app.globalData.access_token = res.data.data.access_token
+                app.globalData.userid = res.data.data.user_id
+                wx.setStorageSync('access_token', res.data.data.access_token)
+                this.getData()
+              }
+            })
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+          }
+        }
+      })
+    }
+  },
+  getData() {
+    util.getData('cards/', {}, res => {
+      if (res.statusCode == 403) {
+        util.reLogin(res => {
+          this.getData()
+        })
+      }
+      this.setData({
+        wallet: res.data.data.card_wallet_items,
+        id: app.globalData.userid,
+      })
+      wx.hideLoading()
+      $loading.hide()
+    })
+  }
+})
